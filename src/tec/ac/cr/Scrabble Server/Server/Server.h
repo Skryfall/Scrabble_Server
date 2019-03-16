d5744af22689cb89a54520d5d759a18b3c36bc86
Kevin Cordero <skryfall@gmail.com>
2019-03-16 17:28:49 -0600

    Initial commit

---
+++ src/tec/ac/cr/Scrabble Server/Server/Server.h
@@ -0,0 +1,20 @@
+//
+// Created by kevin on 16/03/19.
+//
+
+#ifndef SCRABBLE_SERVER_SERVER_H
+#define SCRABBLE_SERVER_SERVER_H
+
+
+class Server {
+
+public:
+
+    Server() = default;
+
+    int run();
+
+};
+
+
+#endif //SCRABBLE_SERVER_SERVER_H
